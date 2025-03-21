c431fd741ce5c509648cd3e72a80b13fa396d583
aiHelpmate <ryanphillippe709@gmail.com>
2025-03-21 16:10:24 +0800

    Initial commit

---
+++ gaode.py
@@ -0,0 +1,77 @@
+import requests
+import pandas as pd
+import os
+
+# Amap API Key
+base_url = "https://restapi.amap.com/v3/place/text"
+api_key = ''
+
+# The number of pages crawled
+page_num = 1
+# Crawl keyword
+bulid_keyword = "茶颜悦色"
+
+# Check whether the Excel file exists
+excel_file_exists = os.path.isfile('city_buildings.xlsx')
+
+'''
+Configuration item End
+'''
+for i in range(1, page_num+1):
+    params = {
+        "key": api_key,
+        "keywords": bulid_keyword,
+        "city": "430100",  # City codes, such as "430100" for Beijing
+        "page": i,
+        "output": "json",
+        "extensions": "all"
+    }
+
+    # Issue a request
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        # Parse the returned JSON data
+        result = response.json()
+
+        # Check for POI data
+        if result['status'] == '1' and 'pois' in result:
+            pois = result['pois']
+            print(len(pois))
+            print(pois[0])
+
+            buildings_data = []
+
+            # Iterate through the POI list, extracting only the required fields
+            for poi in pois:
+                # Check whether the floor information is an empty list. If yes, set the floor to 1
+                floor = poi.get('indoor_data', {}).get('floor', [])
+                if floor == []:
+                    floor = 1
+                poi_data = {
+                    'id': poi.get('id'),  # Use the "get" method to avoid "KeyError"
+                    '名称': poi.get('name'),
+                    '楼层': floor,
+                    '经度': poi.get('location', '').split(',')[0] if 'location' in poi else '',
+                    '纬度': poi.get('location', '').split(',')[1] if 'location' in poi else '',
+                    '类型': poi.get('type')
+                }
+                buildings_data.append(poi_data)
+
+            # Convert the data list to a DataFrame
+            df = pd.DataFrame(buildings_data)
+
+            if excel_file_exists:
+                existing_df = pd.read_excel('city_buildings.xlsx')
+                new_df = pd.concat([existing_df, df], ignore_index=True)
+                new_df.to_excel('city_buildings.xlsx', index=False)
+            else:
+                df.to_excel('city_buildings.xlsx', index=False)
+
+            print("Interested building information has been appended to the Excel file.")
+            excel_file_exists = True
+
+        else:
+            print("No matching building information was found or the request failed.")
+    else:
+        print("Failed to request Amap API, status code:", response.status_code)
---
+++ gaode2.py
@@ -0,0 +1,46 @@
+import requests
+import pandas as pd
+
+api_key = ''
+df = pd.read_excel('city_buildings.xlsx')
+
+# The base URL of the inverse geocoding API
+regeo_url = "https://restapi.amap.com/v3/geocode/regeo"
+
+# Check if the DataFrame already has an 'area' column, if not, add it
+if 'AOI点面积' not in df.columns:
+    df['AOI点面积'] = None
+
+for index, row in df.iterrows():
+    # Check whether the current cell in the 'AOI Point Area 'column already has content, and skip it if it does
+    if pd.notnull(row['AOI点面积']):
+        continue
+    params = {
+        "key": api_key,
+        "location": f"{row['经度']},{row['纬度']}",
+        "extensions": "all"  # Be sure to return all information, including AOI
+    }
+    print(params)
+
+    response = requests.get(regeo_url, params=params)
+    print(response)
+
+    if response.status_code == 200:
+        result = response.json()
+
+        # Check for inverse geocoding data
+        if result['status'] == '1' and 'regeocode' in result:
+            # AOI point area was extracted
+            try:
+                area = result['regeocode']['aois'][0].get('area', '未知')
+                # Update the 'AOI point area' column in the DataFrame
+                df.at[index, 'AOI点面积'] = area
+            except:
+                pass
+        else:
+            print(f"Reverse geocoding request failed:{result['info']}")
+    else:
+        print(f"Failed to request Amap API, status code:{response.status_code}")
+
+df.to_excel('city_buildings.xlsx', index=False)
+print("AOI point area information has been added to the original Excel file.")
